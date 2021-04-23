74c1d65a3a52598b531ff4b4374e8dcd1133a8ac
PJ Fanning <pjfanning@users.noreply.github.com>
2021-04-23 21:42:30 +0100

    Update SwaggerGenerator.scala

--- src/main/scala/com/github/swagger/akka/javadsl/SwaggerGenerator.scala
+++ src/main/scala/com/github/swagger/akka/javadsl/SwaggerGenerator.scala
@@ -3,16 +3,18 @@ package com.github.swagger.akka.javadsl
 import java.util
 
 import com.github.swagger.akka.model.asScala
-import io.swagger.v3.oas.models.ExternalDocumentation
+import io.swagger.v3.oas.models.{Components, ExternalDocumentation}
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.security.{SecurityRequirement, SecurityScheme}
 
 trait SwaggerGenerator {
   def apiClasses: util.Set[Class[_]]
   def apiDocsPath: String = "api-docs"
   def basePath: String = ""
   def info: Info = new Info()
+  def components: util.Optional[Components] = util.Optional.empty()
   def schemes: util.List[String] = util.Collections.singletonList("http")
+  def security: util.List[SecurityRequirement] = util.Collections.emptyList()
   def securitySchemes: util.Map[String, SecurityScheme] = util.Collections.emptyMap()
   def externalDocs: util.Optional[ExternalDocumentation] = util.Optional.empty()
   def vendorExtensions: util.Map[String, Object] = util.Collections.emptyMap()
@@ -30,7 +32,9 @@ private class Converter(javaGenerator: SwaggerGenerator) extends com.github.swag
   override def basePath: String = javaGenerator.basePath
   override def apiDocsPath: String = javaGenerator.apiDocsPath
   override def info: com.github.swagger.akka.model.Info = javaGenerator.info
+  override def components: Option[Components] = asScala(javaGenerator.components)
   override def schemes: List[String] = asScala(javaGenerator.schemes)
+  override def security: List[SecurityRequirement] = asScala(javaGenerator.security)
   override def securitySchemes: Map[String, SecurityScheme] = asScala(javaGenerator.securitySchemes)
   override def externalDocs: Option[ExternalDocumentation] = asScala(javaGenerator.externalDocs)
   override def vendorExtensions: Map[String, Object] = asScala(javaGenerator.vendorExtensions)
